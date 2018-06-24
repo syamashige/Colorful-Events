84f2f33c68803ac2e33f7faf539cd8c20bdfbe26
Sarah <yamashige.sarah@gmail.com>
2018-06-23 17:40:25 -1000

    generating random color

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 
+var colorBox = document.createElement('div');
+colorBox.id = 'randomColor';
+document.body.appendChild(colorBox);
+
+    var colorButton = document.getElementById('colorific');
+    colorButton.addEventListener('click', generateColor);
+
+    function generateColor() {
+
+        function getRandomColor(min, max) {
+            return Math.floor(Math.random() * (max - min +1)) + min;
+        }; 
+
+
+        var colorElem = document.getElementById('randomColor');
+        var r = getRandomColor(0, 225);
+        var g = getRandomColor(0, 225);
+        var b = getRandomColor(0, 225);
+    
+        colorElem.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+    }
+
